4b15cbbe682864c2d5277a2e7a0d8294056bd913
Farhan-SR <farham10670@gmail.com>
2025-05-23 11:42:19 +0600

    2

--- home/views.py
+++ home/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return render(request, "home/index.html")
+
+def success_page(request):
+    print("this is success page")
+    return HttpResponse("<h1> this is success page  </h1>")
+
+
+
+
+
+
+
+
+
+
+
+
